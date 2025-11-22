07e7acf0d19f42e2885763dcf10e26606429e0c0
SoardRaspi <69724218+SoardRaspi@users.noreply.github.com>
2025-11-23 00:39:20 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,15 +1,23 @@
+# from flask import Flask
+
+# # Create an instance of the Flask class.
+# # __name__ is a shortcut for the module's name,
+# # which Flask uses to locate resources like templates.
+# app = Flask(__name__)
+
+# # The route() decorator tells Flask what URL should trigger the function.
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
+
+# # This ensures the Flask application runs only when the script is executed directly.
+# if __name__ == "__main__":
+#     app.run(debug=True) # debug=True enables debug mode, useful for development
+
 from flask import Flask
-
-# Create an instance of the Flask class.
-# __name__ is a shortcut for the module's name,
-# which Flask uses to locate resources like templates.
+ 
 app = Flask(__name__)
-
-# The route() decorator tells Flask what URL should trigger the function.
+ 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
-
-# This ensures the Flask application runs only when the script is executed directly.
-if __name__ == "__main__":
-    app.run(debug=True) # debug=True enables debug mode, useful for development
+    return {"message": "Hello, World!"}
