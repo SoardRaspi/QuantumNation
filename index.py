565fc3a4a59ae02638233aafc801210489b8a310
SoardRaspi <69724218+SoardRaspi@users.noreply.github.com>
2025-11-23 00:45:30 +0530

    Update index.py

--- index.py
+++ index.py
@@ -14,10 +14,16 @@
 # if __name__ == "__main__":
 #     app.run(debug=True) # debug=True enables debug mode, useful for development
 
-from flask import Flask
- 
+from flask import Flask, jsonify
+
 app = Flask(__name__)
- 
+
+
 @app.route("/")
-def hello_world():
-    return {"message": "Hello, World!"}
+def home():
+    return "Flask Vercel Example - Hello World", 200
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({"status": 404, "message": "Not Found"}), 404
